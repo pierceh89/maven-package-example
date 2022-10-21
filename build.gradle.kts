a8d930b5bb28ab721a61f834454f6759ed328089
diego-an <diego.an@kakaoenterprise.com>
2022-10-21 10:44:41 +0900

    github packages 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.6.21"
+    id("maven-publish")
 }
 
 group = "io.github.pierceh89"
@@ -11,4 +12,22 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/pierceh89/maven-package-example")
+            credentials {
+                username = System.getenv("USERNAME")
+                password = System.getenv("TOKEN")
+            }
+        }
+        publications {
+            register<MavenPublication>("gpr") {
+                from(components["java"])
+            }
+        }
+    }
 }
